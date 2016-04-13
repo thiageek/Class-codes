7f6367caefce424f80123a5f01b83810c15a4da0
Thiago Pinheiro <thiagopinheirosilva@gmail.com>
2016-04-12 21:51:48 -0300

    Refatoração do método getParams
    Quero bônus! :D

--- Classes/Class10_Request_Router/control/RequestController.php
+++ Classes/Class10_Request_Router/control/RequestController.php
@@ -21,18 +21,14 @@ class RequestController
 
 	public function getParams($string_params)
 	{
-		$a = str_replace ("?" , "" , $string_params);
-
-		$b = explode("&", $a);
-		
-		$params_map = array();
-
-		foreach ($b as $value) {
-			$c = explode("=", $value);
-
-			$params_map[$c[0]] = $c[1];
+		$string_params = str_replace('?', '', $string_params);
+		$tuples = explode('&', $string_params);
+		$array = array();
+		foreach ($tuples as $tuple) {
+			$array_in = explode('=',$tuple);
+			array_push($array, array($array_in[0] => $array_in[1]));
 		}
-		return $params_map;	
+		return $array;
 	}
 
 
